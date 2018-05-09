48b17723f8241dd4e09b16d20db7ff71b68e5598
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-05-09 17:26:41 -0300

    Atualizando o App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Carousel } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Carousel images={[
+          'https://lorempixel.com/250/250/nature/1',
+          'https://lorempixel.com/250/250/nature/2',
+          'https://lorempixel.com/250/250/nature/3',
+          'https://lorempixel.com/250/250/nature/4',
+          'https://lorempixel.com/250/250/nature/5'
+        ]} />
       </div>
     );
   }
